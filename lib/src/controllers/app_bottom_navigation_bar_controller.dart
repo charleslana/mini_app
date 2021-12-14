3c4be27b28bdbb82c58fde8fb1994ec4d1bbff9d
Charles <charleslanop@gmail.com>
2021-12-13 21:35:42 -0300

    update structure

--- lib/src/controllers/app_bottom_navigation_bar_controller.dart
+++ lib/src/controllers/app_bottom_navigation_bar_controller.dart
@@ -1,11 +1,42 @@
 import 'package:get/get.dart';
+import 'package:mini_app/src/controllers/collection_controller.dart';
+import 'package:mini_app/src/controllers/landing_controller.dart';
 
 class AppBottomNavigationBarController extends GetxController {
   RxInt currentIndex = 2.obs;
+  CollectionController collectionController = Get.put(CollectionController());
+  LandingController landingController = Get.find();
 
   void changeView(int index) {
     if (index != currentIndex.value) {
       currentIndex.value = index;
+      if (index == 0) {
+        collectionController.tabController.index = 0;
+        clearTabHero();
+        clearTabMinis();
+      }
+    }
+  }
+
+  void clearTabHero() {
+    if (landingController.filterHeroesList.length !=
+        landingController.miniModel.heroes.length) {
+      collectionController.heroTextEditingController.clear();
+      landingController.filterHeroesList
+        ..clear()
+        ..addAll(landingController.miniModel.heroes);
+    }
+  }
+
+  void clearTabMinis() {
+    if (landingController.filterMinisList.length !=
+        landingController.miniModel.minis.length) {
+      collectionController
+        ..miniTextEditingController.clear()
+        ..filterIndex.value = 0;
+      landingController.filterMinisList
+        ..clear()
+        ..addAll(landingController.miniModel.minis);
     }
   }
 }

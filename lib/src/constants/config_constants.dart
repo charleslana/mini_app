20df7a40e022ce135a05a891ed6b560d7585b200
Charles <charleslanop@gmail.com>
2022-01-09 12:33:48 -0300

    refactoring

--- lib/src/constants/config_constants.dart
+++ lib/src/constants/config_constants.dart
@@ -1,6 +1,6 @@
 class ConfigConstants {
   static const String baseURL =
-      'https://raw.githubusercontent.com/charleslana/api_mini/master/app_test.json';
+      'https://raw.githubusercontent.com/charleslana/api_mini/master/app.json';
   static const String version = '1.1.1';
   static const String urlDiscord = 'https://discord.gg/tatpFVTzdB';
   static const String urlGitHub = 'https://github.com/charleslana';
